162cd2e5aeec4be369161fcbfdfad6d1ba54dbc0
Mas Bima <bimfikhri@gmail.com>
2025-07-30 08:47:35 +0700

    Update Route

--- routes/api.php
+++ routes/api.php
@@ -5,13 +5,25 @@ use App\Http\Controllers\BukuController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('buku', [BukuController::class, 'index']);
+    Route::get('buku/{id}', [BukuController::class, 'show']);
+    Route::get('buku/slug/{slug}', [BukuController::class, 'showBySlug']);
+    Route::get('buku/statistik/genre', [BukuController::class, 'jumlahPerGenre']);
 
+    Route::get('author', [AuthorController::class, 'index']);
+    Route::get('author/{id}', [AuthorController::class, 'show']);
+    Route::get('author/slug/{slug}', [AuthorController::class, 'showBySlug']);
 
-Route::apiResource('buku', BukuController::class);
-Route::get('buku/slug/{slug}', [BukuController::class, 'showBySlug']);
-Route::get('buku/statistik/genre', [BukuController::class, 'jumlahPerGenre']);
-Route::post('buku/{id}/restore', [BukuController::class, 'restore']);
+    Route::middleware(['role:admin'])->group(function (){
+        Route::post('buku', [BukuController::class, 'store']);
+        Route::put('buku/{buku}', [BukuController::class, 'update']);
+        Route::delete('buku/{buku}', [BukuController::class, 'destroy']);
+        Route::post('buku/{id}/restore', [BukuController::class, 'restore']);
 
-Route::apiResource('author', AuthorController::class);
-Route::get('author/slug/{slug}', [AuthorController::class, 'showBySlug']);
-Route::post('author/{id}/restore', [AuthorController::class, 'restore']);
+        Route::post('author', [AuthorController::class, 'store']);
+        Route::put('author/{author}', [AuthorController::class, 'update']);
+        Route::delete('author/{author}', [AuthorController::class, 'destroy']);
+        Route::post('author/{id}/restore', [AuthorController::class, 'restore']);
+    });
+});
