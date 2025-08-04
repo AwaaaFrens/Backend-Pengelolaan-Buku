1d9c46947ee9c1fe1c2ea0ab3d73bbfafd6abc94
Mas Bima <bimfikhri@gmail.com>
2025-08-04 23:29:46 +0700

    Menambahkan UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\ApiResponseHelper;
+use App\Http\Requests\UpdateUserRequest;
 use App\Services\UserService;
 use Illuminate\Support\Facades\Request;
 
@@ -30,11 +31,49 @@ class UserController extends Controller
         $user = $this->userService->getUserById($id);
 
         if (!$user) {
+            return ApiResponseHelper::error('Gagal mengambil data', 400);
+        }
+
+        return ApiResponseHelper::success($user, 'Berhasil mengambil data');
+    }
+
+    public function update(UpdateUserRequest $request, $id)
+    {
+        $user = $this->userService->updateUser($id, $request);
+
+        if (!$user) {
             return ApiResponseHelper::error('Gagal memperbarui data', 400);
         }
 
         return ApiResponseHelper::success($user, 'Berhasil memperbarui data');
     }
 
-    public function update()
+    public function destroy($id)
+    {
+        $deleted = $this->userService->deleteUser($id);
+
+        if (!$deleted) {
+            return ApiResponseHelper::error('Gagal menghapus data', 400);
+        }
+
+        return ApiResponseHelper::success(null, 'Berhasil menghapus data');
+    }
+
+    public function toggleStatusUsers($id)
+    {
+        $user = $this->userService->toggleUserStatus($id);
+
+        if (!$user) {
+            return ApiResponseHelper::error('User tidak ditemukan', 401);
+        }
+
+        return ApiResponseHelper::success($user, 'Status user berhasil diubah');
+    }
+
+    public function statistics()
+    {
+        $stats = $this->userService->getUserStats();
+
+        return ApiResponseHelper::success($stats, 'Stats user berhasil ditampilkan');
+    }
 }
